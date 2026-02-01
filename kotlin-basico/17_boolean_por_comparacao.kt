da2bee62e562eeb30c1f38f6aa8aec11bd3997b8
Alex Cardoso Bento <programadorbento@gmail.com>
2026-02-01 13:37:46 -0300

    Create 17_boolean_por_comparacao.kt
    Programa que cria uma variável Boolean a partir de uma comparação numérica e utiliza essa variável para controlar o fluxo do programa.
    Esse padrão é fundamental em sistemas reais, onde condições são calculadas antes de decidir ações.

---
+++ kotlin-basico/17_boolean_por_comparacao.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val idade = 18
+    val maiorDeIdade = idade >= 18
+
+    if (maiorDeIdade) {
+        println("Acesso liberado")
+    } else {
+        println("Acesso negado")
+    }
+}
