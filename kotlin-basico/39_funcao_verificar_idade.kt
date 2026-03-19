9032c335f6c7219187f023d29db87feeea0507db
Alex Cardoso Bento <programadorbento@gmail.com>
2026-03-19 11:59:53 -0300

    Create 39_funcao_verificar_idade.kt
    Este exemplo demonstra a criação e uso de uma função em Kotlin para encapsular lógica condicional.
    
    A função verificarIdade recebe um valor inteiro como parâmetro e realiza uma verificação simples para determinar se a pessoa é maior ou menor de idade.
    
    Regras utilizadas:
    
    Maior de idade → idade maior ou igual a 18
    
    Menor de idade → idade menor que 18
    
    No main, a função é chamada passando um valor diretamente como argumento.
    
    O exercício reforça conceitos importantes como:
    
    Criação de funções com parâmetros (fun verificarIdade(a: Int))
    
    Separação de responsabilidades (lógica fora do main)
    
    Reutilização de código
    
    Uso de estruturas condicionais dentro de funções
    
    Esse padrão é fundamental para evolução em programação, pois permite organizar melhor o código e preparar para sistemas maiores, como no desenvolvimento de jogos e aplicações.

---
+++ kotlin-basico/39_funcao_verificar_idade.kt
@@ -0,0 +1,15 @@
+fun verificarIdade(a: Int){
+            
+            if (a >= 18) {
+                
+                println("Maior de idade")
+            } else {
+                
+                println("menor de idade")
+            }
+    
+}
+
+fun main() {
+    verificarIdade(3)
+}
